e337e9d8b1f801bca2b761a00be678190135644c
natsuki <natsuki.uji410@gmail.com>
2023-08-12 00:58:42 +0900

    課題3-1完了しました

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::orderBy('id', 'desc')->get();
+        return view('tasks.index',compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $comment = $request->input('comment');
+        $task = new Task();
+        $task->comment = $comment;
+        $task->status = '作業中'; 
+        $task->save();
+    
+        return redirect()->route('tasks.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDoリスト</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <h1>ToDoリスト</h1>
+    <form method="GET" action="{{ route('tasks.index') }}">
+        <input type="radio" name="status" value="all" id="all" checked>
+        <label for="all">すべて</label>
+        <input type="radio" name="status" value="作業中" id="working">
+        <label for="working">作業中</label>
+        <input type="radio" name="status" value="完了" id="completed">
+        <label for="completed">完了</label>
+    </form>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>コメント</th>
+            <th>状態</th>
+        </tr>
+        @foreach($tasks->sortBy('id') as $task)
+            <tr>
+                <td>{{ $task->id }}</td>
+                <td>{{ $task->comment }}</td>
+                <td>
+                    <button>作業中</button>
+                    <button>削除</button>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    <h2>新規タスクの追加</h2>
+    <form method="POST" action="{{ route('tasks.store') }}">
+        @csrf
+        <input type="text" name="comment">
+        <button type="submit">追加</button>
+    </form>
+    
+</body>
+</html>
