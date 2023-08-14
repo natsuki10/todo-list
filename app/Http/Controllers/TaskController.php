0f58806d59dee194d08654a01bd643ab7b06f254
natsuki <natsuki.uji410@gmail.com>
2023-08-14 12:47:04 +0900

    課題3-1修正しました

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,14 +17,6 @@ class TaskController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -36,37 +28,5 @@ class TaskController extends Controller
         $task->save();
     
         return redirect()->route('tasks.index');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 use App\Http\Controllers\TaskController;
-Route::resource('tasks', TaskController::class);
+Route::resource('tasks', TaskController::class,['only'=>['index','store']]);
